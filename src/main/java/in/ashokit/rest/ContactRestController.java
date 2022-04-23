c8e814bd2b51598556eddb314036020b4f608ea0
avinash gupta <avinashgupta@avinashs-MacBook-Air.local>
2022-04-24 00:19:32 +0530

    modified the datasource properties in application.properties file

--- src/main/java/in/ashokit/rest/ContactRestController.java
+++ src/main/java/in/ashokit/rest/ContactRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ashokit.entity.Contact;
@@ -21,7 +22,7 @@ public class ContactRestController {
 	private ContactService service;
 	
 	@PostMapping("/contact")
-	public ResponseEntity<String> contact (Contact contact){
+	public ResponseEntity<String> contact(@RequestBody Contact contact){
 		String status = service.upsert(contact);
 		return new ResponseEntity<>(status,HttpStatus.CREATED);
 	}
@@ -32,13 +33,13 @@ public class ContactRestController {
 		 return new ResponseEntity<>(allContacts, HttpStatus.OK);
 	}
 
-	@GetMapping("/contacts/{cid}")
+	@GetMapping("/contact/{cid}")
 	public ResponseEntity<Contact> getContact(@PathVariable int cid){
 		Contact contact= service.getContact(cid);
 		return new ResponseEntity<>(contact, HttpStatus.OK);
 	}
 	
-	@DeleteMapping("/contact/{id}")
+	@DeleteMapping("/contact/{cid}")
 	public ResponseEntity<String> deleteContact(@PathVariable int cid){
 		String deleteContact= service.deleteContact(cid);
 		return new ResponseEntity<>(deleteContact, HttpStatus.OK);
